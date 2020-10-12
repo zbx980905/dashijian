d3dd81786de64b6ecb6aae375c9c54c67d71dd36
张博鑫 <2953532359@qq.com>
2020-10-12 14:33:05 +0800

    完成了登录和注册功能

---
+++ assets/js/login.js
@@ -0,0 +1,62 @@
+$(function () {
+    // 点击去“注册账号”链接
+    $('#link_reg').on('click', function () {
+        $('.reg-box').hide()
+        $('.login-box').show()
+    })
+    // 点击“去登录”链接
+    $('#link_login').on('click', function () {
+        $('.login-box').hide()
+        $('.reg-box').show()
+    })
+    // 从layui 获取form对象
+    var form = layui.form;
+    var layer = layui.layer;
+    // 通过 form.verify() 函数自定义校验规则
+    form.verify({
+        // 自定义了一个叫做pwd校验规则
+        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+        repwd: function (value) {
+            var pwd = $('.reg-box [name=password]').val()
+            if (pwd !== value) {
+                return "两次密码不一致"
+            }
+        }
+    })
+    // 监听注册表单的提交事件
+    $('#form_reg').on('submit', function (e) {
+        // 阻止默认提交行为
+        e.preventDefault()
+        // 发起ajax的POST请求
+        var data = {
+            username: $('#form_reg [name = username]').val(),
+            password: $('#form_reg [name = password]').val()
+        }
+        $.post('/api/reguser', data, function (res) {
+            if (res.status !== 0) {
+                return layer.msg(res.message)
+            }
+            layer.msg('注册成功，请登录！')
+            // 模拟点击行为
+            $('#link_login').click()
+        })
+    })
+    //监听登录表单的提交事件
+    $('#form_login').submit(function (e) {
+        e.preventDefault()
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg('登录失败！')
+                }
+                layer.msg('登录成功！')
+                localStorage.setItem('token', res.token)
+                //跳转到后台主页
+                location.href = '/index.html'
+            }
+        })
+    })
+})
